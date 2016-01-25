210cb2e2889fa8a80178ebc83b9f75ecffcfa13a
KSXGitHub <hvksmr1996@gmail.com>
2016-01-25 20:59:10 +0700

    update

--- index.js
+++ index.js
@@ -3,12 +3,25 @@
 	'use strict';
 
 	var XIterable = require('x-iterable/create-class');
+	var Element = require('./element.js');
+
+	var _key_iterator = Symbol.iterator;
 
 	class PureObjectIterable {
-		
+
+		constructor(object) {
+			this.object = object;
+		}
+
+		* [_key_iterator]() {
+			
+		}
+
 	};
 
 	class ObjectIterable extends XIterable(PureObjectIterable) {};
+
+	ObjectIterable.PureObjectIterable = ObjectIterable.Pure = ObjectIterable.Base = PureObjectIterable;
 
 	module.exports = ObjectIterable;
 
