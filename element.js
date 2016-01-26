d0ac029aa1ac2dcbc3d8f84eeb49102e7710e022
KSXGitHub <hvksmr1996@gmail.com>
2016-01-26 11:33:06 +0700

    add element.js

--- element.js
+++ element.js
@@ -2,10 +2,34 @@
 ((module) => {
 	'use strict';
 
-	class Element {
+	class Element extends Array {
+
 		constructor(object, property) {
-			
+			super(object, property);
 		}
+
+		get object() {
+			return this[0];
+		}
+
+		get property() {
+			return this[1];
+		}
+
+		get descriptor() {
+			return Object.getOwnPropertyDescriptor(this.object, this.property);
+		}
+
+		get value() {
+			return this.object[this.property];
+		}
+
+		set value(value) {
+			this.object[this.property] = value;
+		}
+
 	}
+
+	module.exports = class extends Element {};
 
 })(module);
