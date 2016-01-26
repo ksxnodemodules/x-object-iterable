3ab5e2d155e458182fe25ed714d81596f0c7d70e
KSXGitHub <hvksmr1996@gmail.com>
2016-01-26 12:26:42 +0700

    complete test

--- test/index.js
+++ test/index.js
@@ -4,6 +4,25 @@
 
 	var ObjectIterable = require('x-object-iterable');
 
-	
+
+	var object = {
+		data: 12,
+		get accessor() {
+			return Math.random();
+		}
+	};
+
+	var clone = {};
+
+	var iterable = new ObjectIterable(object);
+
+	for (let element of iterable) {
+		Object.defineProperty(clone, element.property, element.descriptor);
+	}
+
+	module.exports = {
+		clone: clone,
+		list: iterable.toArray()
+	};
 
 })(module);
