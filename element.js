251beeb34f2cdad925065fde95b8822867aea1fa
khai96_ <hvksmr1996@gmail.com>
2017-04-07 21:36:34 +0700

    No wrapper here, too

--- element.js
+++ element.js
@@ -1,32 +1,29 @@
+'use strict'
 
-((module) => {
-  'use strict'
-
-  class Element extends Array {
-    get object () {
-      return this[0]
-    }
-
-    get property () {
-      return this[1]
-    }
-
-    get descriptor () {
-      return Object.getOwnPropertyDescriptor(this.object, this.property)
-    }
-
-    get value () {
-      return this.object[this.property]
-    }
-
-    set value (value) {
-      this.object[this.property] = value
-    }
-
-    valueOf () {
-      return this.value
-    }
+class Element extends Array {
+  get object () {
+    return this[0]
   }
 
-  module.exports = class extends Element {}
-})(module)
+  get property () {
+    return this[1]
+  }
+
+  get descriptor () {
+    return Object.getOwnPropertyDescriptor(this.object, this.property)
+  }
+
+  get value () {
+    return this.object[this.property]
+  }
+
+  set value (value) {
+    this.object[this.property] = value
+  }
+
+  valueOf () {
+    return this.value
+  }
+}
+
+module.exports = class extends Element {}
